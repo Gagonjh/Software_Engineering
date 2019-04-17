be1d492eda0f4e47612abd90f257d207d73b31bb
Gagonjh <49719775+Gagonjh@users.noreply.github.com>
2019-04-17 17:33:59 +0200

    Create test_isWellSorted.java

---
+++ src/jUnit_test/test_isWellSorted.java
@@ -0,0 +1,14 @@
+package jUnit_test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class test_isWellSorted {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
